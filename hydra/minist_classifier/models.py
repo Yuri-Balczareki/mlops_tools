57f3877dc5c4b4461b8b81eb6e389e2a8f1e4d14
Yuri <yuri.balczareki@hotmail.com>
2024-09-27 11:41:41 -0300

    Added a second Model CIFAR10 to this mini project

--- hydra/minist_classifier/models.py
+++ hydra/minist_classifier/models.py
@@ -18,4 +18,17 @@ class SimpleModel(nn.Module):
         x = self.head(x)
         return x
 
-    
+class CIFAR10Model(nn.Module):
+    def __init__(self, backbone: nn.Module, adapter: nn.Module, head: nn.Module) -> None:
+        super().__init__()
+        self.backbone = backbone
+        self.adapter = adapter
+        self.head = head
+
+    def forward(self, x: Tensor) -> Tensor:
+        # x = x.repeat(1, 3, 1, 1) #CIFAR10 images are already RGB
+        x = self.backbone(x)
+        x = self.adapter(x)
+        x = self.head(x)
+        return x
+
--- hydra/minist_classifier/tasks.py
+++ hydra/minist_classifier/tasks.py
@@ -1,9 +1,8 @@
+from torchmetrics import Accuracy
+from torch import nn, Tensor
 from typing import Callable, Iterator
 import pytorch_lightning as pl
-from torch import Tensor, optim
-
-from torchmetrics import Accuracy
-from torch import nn
+from torch import optim
 
 PARTIAL_OPTIMIZER_TYPE = Callable[[Iterator[nn.Parameter]], optim.Optimizer]
 
@@ -45,6 +44,44 @@ class MNISTClassification(TrainingTask):
         preds = self(images)
         self.validaiton_accuracy(preds, labels)
         self.log("validation_accuracy", self.validaiton_accuracy, on_step=False, on_epoch=True)
+
+    def test_step(self, batch, batch_idx):
+        images, labels = batch
+        preds = self(images)
+        self.test_accuracy(preds, labels)
+        self.log("test_accuracy", self.test_accuracy, on_step=False, on_epoch=True)
+
+
+class CIFAR10Classification(TrainingTask):
+    def __init__(self, model: nn.Module, optimizer: PARTIAL_OPTIMIZER_TYPE, loss_function: nn.Module) -> None:
+        super().__init__(optimizer)
+
+        self.model = model
+        self.loss_function = loss_function
+
+        # CIFAR-10 has 10 classes
+        nrof_classes = 10
+        self.train_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+        self.validation_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+    
+    def forward(self, x) -> Tensor: 
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx) -> Tensor: 
+        images, labels = batch 
+        logits = self(images)
+        loss = self.loss_function(logits, labels)
+        self.train_accuracy(logits, labels)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        self.log("train_accuracy", self.train_accuracy, on_step=False, on_epoch=True)
+        return loss    
+    
+    def validation_step(self, batch, batch_idx): 
+        images, labels = batch 
+        preds = self(images)
+        self.validation_accuracy(preds, labels)
+        self.log("validation_accuracy", self.validation_accuracy, on_step=False, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
         images, labels = batch
